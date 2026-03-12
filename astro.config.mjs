d5a7017f13f9ad6e763a94bf9d9d4ced56e6926b
levinismynameirl <levin.mircea@gmail.com>
2026-03-12 06:52:46 +0200

    change astro config (will revert this ig)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-	site: 'https://libs.advaith.io'
+	site: 'https://levinismynameirl.github.io',
+	base: '/libs-fork',
 })
